da99ee869c8cca7989c2432926e7302e6b87366e
Kandrianaha <k.andria@icloud.com>
2025-08-09 06:06:31 +0000

    betty code

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,14 +3,14 @@
 
 /**
  * binary_tree_insert_right - inserts a node as the right-child of another node
- * @parent: pointer to the node to insert the irght-child in
+ * @parent: pointer to the node to insert the right-child in
  * @value: value to store in the new node
- * Return: pointer to the created node, NULL on failure or if parent is NULL
+ * Return: pointer to the created node, NULL on failuree or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	if (parent == NULL)
 		return (NULL);
 
@@ -33,3 +33,4 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	return (new_node);
 }
+
