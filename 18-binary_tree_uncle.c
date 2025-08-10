becb215a853aa6c04ed5df8723ceab7af8470856
Kandrianaha <k.andria@icloud.com>
2025-08-10 09:07:46 +0000

    uncle of node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,11 +7,15 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *grandparent;
+
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	if (node->parent == node->parent->parent->left)
-		return (node->parent->parent->right);
+	grandparent = node->parent->parent;
+
+	if (grandparent->left == node->parent)
+		return (grandparent->right);
 	else
-		return (node->parent->parent->parent->left);
+		return (grandparent->left);
 }
