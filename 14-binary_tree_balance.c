c13539308e8057fe605dedae2ae9bc564812c52d
Kandrianaha <k.andria@icloud.com>
2025-08-10 08:38:31 +0000

    betty style

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,8 +11,8 @@ size_t binary_tree_height_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height =binary_tree_height_balance(tree->left);
-	right_height=binary_tree_height_balance(tree->right);
+	left_height = binary_tree_height_balance(tree->left);
+	right_height = binary_tree_height_balance(tree->right);
 
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
@@ -30,7 +30,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	left_height = (tree->left) ? (int)binary_tree_height_balance(tree->left) : 0;
-	right_height = (tree->right) ? (int)binary_tree_height_balance(tree->right) : 0;
+	right_height = (tree->right) ?
+		(int)binary_tree_height_balance(tree->right) : 0;
 
 	return (left_height - right_height);
 }
